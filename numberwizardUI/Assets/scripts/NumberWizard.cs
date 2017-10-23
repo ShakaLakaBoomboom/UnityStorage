bd307f03af865a5639214ca1f135077f7045dd4f
ShakaLakaBoomboom <32704574+ShakaLakaBoomboom@users.noreply.github.com>
2017-10-23 13:58:00 +0200

    firstUI

---
+++ numberwizardUI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Use this for initialization
+    int maxnumber = 1000;//variable for largest number, this is a global variable as it is out of start function
+    int minnumber = 1;//variable for smallest number, int is the type and minnumber is the name
+    int guess=500;//variable that holds the middle value
+    int maxNumofGuesses = 3;
+
+    public Text guessText;
+
+    public void GuessHigher() {
+        minnumber = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        maxnumber = guess;
+        NextGuess();
+    }
+
+    void StartGame() {
+        print("welcome to a game of Number Wizard"); //console will display what is between the ""
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess=Random.Range(minnumber, maxnumber + 1) ;
+        //conv guess to string and updates text
+        guessText.text = guess.ToString();
+        maxNumofGuesses--;
+        if (maxNumofGuesses == 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+
+    }
+
+    void Start () {
+        guess = Random.Range(minnumber, maxnumber + 1);
+        guessText.text = guess.ToString();
+        
+	}
+	
+	// Update is called once per frame
+
+}
---
+++ numberwizardUI/Assets/scripts/levelmanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelmanager : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+    {
+        print("loading level" + levelName);
+        SceneManager.LoadScene(levelName);
+
+    }
+
+    private void Start()
+    {
+        LoadLevel("Level 1");
+    }
+    public void Quit()
+    {
+        print("caw ja bicca peanut");
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
